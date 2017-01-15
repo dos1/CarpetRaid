768dc8041546d61d6db3fd2d487759be7e1461fb
Sebastian Krzyszkowiak <dos@dosowisko.net>
2017-01-15 02:14:40 +0100

    some work on gui

--- game.gd
+++ game.gd
@@ -9,10 +9,19 @@ func _input(event):
 			self.get_tree().set_input_as_handled()
 			OS.set_window_fullscreen(!OS.is_video_mode_fullscreen())
 
+func _process(delta):
+	var fuel = Globals.get('fuel')
+	if fuel > 1000:
+		fuel = 1000
+	if fuel < 0:
+		fuel = 0
+	Globals.set('fuel', fuel)
+	
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	Globals.set('score', 0)
 	Globals.set('lifes', 3)
-	Globals.set('fuel', 100)
+	Globals.set('fuel', 1000)
 	self.set_process_input(true)
+	self.set_process(true)
