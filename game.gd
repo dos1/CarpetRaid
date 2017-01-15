01e795be5b4df319f25c0798fa63a4fae7406564
Sebastian Krzyszkowiak <dos@dosowisko.net>
2017-01-15 10:51:54 +0100

    lifes

--- game.gd
+++ game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var old_score = 0
+
 func _input(event):
 	if (event.type == InputEvent.KEY) and (event.pressed):
 		if (event.scancode == KEY_ESCAPE):
@@ -10,6 +12,11 @@ func _input(event):
 			OS.set_window_fullscreen(!OS.is_video_mode_fullscreen())
 
 func _process(delta):
+	var score = Globals.get('score')
+	if int(score/2000) > int(self.old_score/2000):
+		Globals.set('lifes', Globals.get('lifes')+1)
+	self.old_score = score
+	
 	var fuel = Globals.get('fuel')
 	if fuel > 1000:
 		fuel = 1000
